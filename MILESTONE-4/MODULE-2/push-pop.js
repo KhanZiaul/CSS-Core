98917c215d1c4a4ff67b894f54d3b1a0ab9db533
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 01:00:26 +0600

    updated

--- MILESTONE-4/MODULE-2/push-pop.js
+++ MILESTONE-4/MODULE-2/push-pop.js
@@ -1,3 +1,19 @@
+var names=['megha', 'moni', 'barai'];
+
+console.log(names);
+
+//add new items in last of an array
+
+names.push('zia');
+console.log(names);
+
+
+//pop last items from an array
+
+names.pop();
+console.log(names);
+
+
 //-------------------------------------->
 
 var ages= [10, 50, 6, 90, 60];
