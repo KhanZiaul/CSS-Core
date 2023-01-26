7d7a73ae49a3765ce64543117ab2f355ea6e73fe
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 00:02:40 +0600

    updated

--- MILESTONE-4/MODULE-2/introduction_array.js
+++ MILESTONE-4/MODULE-2/introduction_array.js
@@ -1,8 +1,11 @@
-var name=['megha', 'moni', 'barai', 'zia'];
-var ages=[10, 50, 6, 90, 60];
+var names=['megha', 'moni', 'barai', 'zia'];
+
+var ages= [10, 50, 6, 90, 60];
 
 console.log(name);
 
 console.log(ages);
 
-console.log(name.length);
+console.log(names.length);
+
+console.log(ages.length);
