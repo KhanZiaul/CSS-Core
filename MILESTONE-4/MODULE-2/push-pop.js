54de42bf814e9832ebec65defaa887e1577e57cd
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 00:53:53 +0600

    push-pop.js

---
+++ MILESTONE-4/MODULE-2/push-pop.js
@@ -0,0 +1,17 @@
+//-------------------------------------->
+
+var ages= [10, 50, 6, 90, 60];
+
+console.log(ages);
+
+//add new items in last of an array
+
+
+ages.push(40);
+console.log(ages);
+            
+//pop last items from an array
+
+ages.pop();
+console.log(ages);
+
