61008fde1510bf98adf028347197fc7626534c53
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-01-27 14:24:03 +0600

    problem-2 updated

--- MILESTONE-4/MODULE-2/Problems/problem-2.js
+++ MILESTONE-4/MODULE-2/Problems/problem-2.js
@@ -8,4 +8,143 @@
 // 39 or less => F grade
 // Write a program to find your and your friends’ grades using if-else;
 
+var khan_zia = 85;
 
+var Tom = 66 ;
+
+var Jane = 95 ;
+
+var Peter = 56 ;
+
+var John = 39 ;
+
+// Khan Zia Score Grade
+
+if(khan_zia >= 80 ){
+
+    console.log('KHAN ZIA Score Is A');
+}
+else if (khan_zia >= 60){
+
+    console.log('KHAN ZIA Score Is B');
+
+}
+else if (khan_zia >= 50){
+
+    console.log('KHAN ZIA Score Is C');
+
+}
+else if(khan_zia >= 40){
+
+    console.log('KHAN ZIA Score Is D');
+
+}
+else if(khan_zia <= 39){
+
+    console.log('KHAN ZIA Score Is F');
+}
+
+// Tom Score Grade
+
+if(Tom >= 80 ){
+
+    console.log('Tom Score Is A');
+}
+else if (Tom >= 60){
+
+    console.log('Tom Score Is B');
+
+}
+else if (Tom >= 50){
+
+    console.log('Tom Score Is C');
+
+}
+else if(Tom >= 40){
+
+    console.log('Tom Score Is D');
+
+}
+else if(Tom <= 39){
+
+    console.log('Tom Score Is F');
+}
+
+// Jane Score Grade
+
+if(Jane >= 80 ){
+
+    console.log('Jane Score Is A');
+}
+else if (Jane >= 60){
+
+    console.log('Jane Score Is B');
+
+}
+else if (Jane >= 50){
+
+    console.log('Jane Score Is C');
+
+}
+else if(Jane >= 40){
+
+    console.log('Jane Score Is D');
+
+}
+else if(Jane <= 39){
+
+    console.log('Jane Score Is F');
+}
+
+// Peter Score Grade
+
+if(Peter >= 80 ){
+
+    console.log('Peter Score Is A');
+}
+else if (Peter >= 60){
+
+    console.log('Peter Score Is B');
+
+}
+else if (Peter >= 50){
+
+    console.log('Peter Score Is C');
+
+}
+else if(Peter >= 40){
+
+    console.log('Peter Score Is D');
+
+}
+else if(Peter <= 39){
+
+    console.log('Peter Score Is F');
+}
+
+
+// John Score Grade
+
+if(John >= 80 ){
+
+    console.log('John Score Is A');
+}
+else if (John >= 60){
+
+    console.log('John Score Is B');
+
+}
+else if (John >= 50){
+
+    console.log('John Score Is C');
+
+}
+else if(John >= 40){
+
+    console.log('John Score Is D');
+
+}
+else if(John <= 39){
+
+    console.log('John Score Is F');
+}
