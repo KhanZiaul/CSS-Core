dd2ab6b76ed10ef78665021a654255a2dc990629
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-01 22:45:04 +0600

    indexof done

---
+++ MILESTONE-4/MODULE-7/indexOf startswith endswith.js
@@ -0,0 +1,13 @@
+// indexOf , startswith and endswith
+
+//indexOf------------------->
+
+const song = "sada sada kala kala";
+
+if (song.indexOf('kala kala') !== -1){
+    console.log('it is valid');
+}
+
+else{
+    console.log('it is not valid');
+}
