91d9d41ca35ddafb2cb377987b0e31d25ae8cc24
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-02 01:48:07 +0600

    destructing done

--- MILESTONE-4/MODULE-7/swap.js
+++ MILESTONE-4/MODULE-7/swap.js
@@ -2,17 +2,25 @@
 
 // with temp
 
+// let a = 7;
+
+// let b = 8;
+
+// let temp = a;
+
+// a = b;
+
+// b= temp;
+
+// console.log(a, b);
+
+// Destructing
+
 let a = 7;
 
-let b = 8;
+let b = 9;
 
-let temp = a;
+[a , b] = [b , a];
 
-a = b;
-
-b= temp;
-
-console.log(a, b);
-
-// without temp
+console.log(a,b);
 
