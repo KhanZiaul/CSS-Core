575a18b25cae6555e7a620f8c17e35e3572cac73
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-02 00:17:50 +0600

    substring , slice , substr done

--- MILESTONE-4/MODULE-7/split,slice,substr,substring ,concate,join.js
+++ MILESTONE-4/MODULE-7/split,slice,substr,substring ,concate,join.js
@@ -9,3 +9,13 @@ console.log(song.split(' '));
 console.log(song.split(''));
 
 console.log(song.split('.'));
+
+// slice , substr , substring---------------------->
+
+const biodata = 'there i s no biodata';
+
+console.log(biodata.slice(3,5));
+
+console.log(biodata.substr(3,7));
+
+console.log(biodata.substring(3,5));
