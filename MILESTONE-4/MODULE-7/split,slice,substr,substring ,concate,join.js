1effdf9798f668fbfffaee025e10f0ef174905c1
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-01 23:18:37 +0600

    split done

---
+++ MILESTONE-4/MODULE-7/split,slice,substr,substring ,concate,join.js
@@ -0,0 +1,11 @@
+// split , slice , substr , substring , concate , join
+
+// split --------------->
+
+const song = "this is.split string"
+
+console.log(song.split(' '));
+
+console.log(song.split(''));
+
+console.log(song.split('.'));
