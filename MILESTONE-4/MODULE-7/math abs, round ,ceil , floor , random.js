eb5d1b48ff4b72975999ef5645b8dba13001f27c
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-02 01:11:32 +0600

    power and absolute done

---
+++ MILESTONE-4/MODULE-7/math abs, round ,ceil , floor , random.js
@@ -0,0 +1,38 @@
+// math pow , abs , round , ceil , floor , random
+
+// power ------------------>
+
+console.log(Math.pow(3,2));
+
+// absolute ---------------->
+
+// normally
+const num1 = 15;
+
+const num2 = 20;
+
+if( num1-num2 >2){
+
+    console.log('you are hot');
+}
+
+else{
+
+    console.log('you are not');
+}
+
+// use absolute
+
+const num3 = 15;
+
+const num4 = 20;
+
+if( Math.abs(num3-num4) > 2){
+
+    console.log('you are hot');
+}
+
+else{
+
+    console.log('you are not');
+}
