3e6738b8f56338bb000e9feb5719f3643c06e323
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-02-02 01:13:59 +0600

    round , ceil and floor done

--- MILESTONE-4/MODULE-7/math abs, round ,ceil , floor , random.js
+++ MILESTONE-4/MODULE-7/math abs, round ,ceil , floor , random.js
@@ -36,3 +36,13 @@ else{
 
     console.log('you are not');
 }
+
+// round , ceiling and floor -------------------->
+
+let x = 2.1416;
+
+console.log(Math.round(x));
+
+console.log(Math.ceil(x));
+
+console.log(Math.floor(x));
